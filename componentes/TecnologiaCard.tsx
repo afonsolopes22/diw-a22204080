a0ba228fe73f628b4e4cb89873241f6b79fb0afb
afonso-lopes-a22204080 <100075108+afonsolopes22@users.noreply.github.com>
2025-12-04 01:42:19 +0000

    ..

---
+++ componentes/TecnologiaCard.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+interface TecnologiaCardProps {
+  title: string;
+  image: string;
+}
+
+export default function TecnologiaCard({ title, image }: TecnologiaCardProps) {
+  return (
+    <div className="w-40 h-40 bg-white border rounded-xl shadow flex flex-col items-center justify-center p-3">
+      <Image
+        src={`/tecnologias/${image}`}
+        alt={title}
+        width={64}
+        height={64}
+      />
+      <p className="mt-2 text-sm font-medium text-center">{title}</p>
+    </div>
+  );
+}
